d6213960ac1a543318498d2fb009b1b7f84e5ce9
Lukas Hoppe <lukashoppe@Lukass-MBP.fritz.box>
2019-12-19 22:59:49 +0100

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"Excyted - We create","short_name":"Excyted","start_url":"/","background_color":"#fff","theme_color":"#000","display":"minimal-ui","icon":"src/images/favicon.png"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
