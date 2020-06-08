78a440594230a87dba2a80020e12ea476a1a548d
Antonio Schmitter <antonio@excyted.io>
2020-06-08 21:17:51 +0200

    added gatsby-image

--- src/components/homepage/banner.js
+++ src/components/homepage/banner.js
@@ -1,89 +1,133 @@
 import React from 'react';
+import Img from 'gatsby-image';
+import { graphql, useStaticQuery } from 'gatsby';
 import './banner.scss';
 import { Container, Row, Col } from 'react-bootstrap';
-import PremiumButton from '../general/PremiumButton';
-import logo from '../../images/banner/logo.png';
 import { Parallax } from 'react-scroll-parallax';
-import StackWhu from '../../images/content/whu.jpg';
 import Triangle from '../../images/content/triangle.svg';
 import Dots from '../../images/content/dots.png';
 import Code1 from '../../images/content/code1.png';
 import Rectangles from './rectangles';
 import { Link } from 'gatsby';
 
-const Banner = () => (
-	<section id='banner'>
-		<Container>
-			<Row className='no-padding-top'>
-				<Col md={6} className='banner-text'>
-					<div className='typewriter-container'>
-						<h2 className='typewriter'>Brand. Digital. and Motion.</h2>
-					</div>
-					<img src={logo} alt='Logo' className='banner-logo' />
+const Banner = () => {
+	const { logo, whu } = useStaticQuery(graphql`
+		query {
+			logo: file(relativePath: { eq: "banner/logo.png" }) {
+				childImageSharp {
+					fluid(quality: 100, maxWidth: 540) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			whu: file(relativePath: { eq: "content/whu.jpg" }) {
+				childImageSharp {
+					fluid(quality: 100, maxWidth: 506) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+		}
+	`);
 
-					<h3>Creative Studio</h3>
-					<p>
-						Als eine hybride Medienproduktion entwickeln wir kreative Lösungen
-						für deine Marke, dein Unternehmen oder dein Projekt. Mit excyted
-						bringen wir unsere Disziplinen aus Web Development und der
-						Produktion von Medien zusammen und können so nicht nur konzeptionell
-						sondern auch technisch und grafisch eng mit Dir an deiner Idee
-						basteln. Bock zu excyten?
-						<br />
-						Let's get in touch!{' '}
-					</p>
-					<Link to='/projects' className='premium-button'>
-						<span className='module'>Unsere Projekte</span>
-					</Link>
-				</Col>
-				<Col md={6} className='parallaxfull'>
-					<div className='parallax-stack'>
-						<div>
-							<Parallax y={[0, 0]} x={[0, 10]} tagOuter='figure'>
-								<div className='imgequi'>
-									<img src={Dots} alt='' className='dots' />
-								</div>
-							</Parallax>
+	return (
+		<section id='banner'>
+			<Container>
+				<Row className='no-padding-top'>
+					<Col md={6} className='banner-text'>
+						<div className='typewriter-container'>
+							<h2 className='typewriter'>Brand. Digital. and Motion.</h2>
 						</div>
-						<div>
-							<Parallax y={[0, -80]} x={[0, 0]} tagOuter='figure'>
-								<img src={Code1} alt='' className='code1' />
-							</Parallax>
-						</div>
-						<div>
-							<Parallax y={[0, 10]} x={[0, -10]} tagOuter='figure'>
-								<img src={StackWhu} alt='' className='opacity3' />
-							</Parallax>
-						</div>
-						<div>
-							<Parallax y={[0, 20]} x={[0, -20]} tagOuter='figure'>
-								<img src={StackWhu} alt='' className='opacity2' />
-							</Parallax>
-						</div>
-						<div>
-							<Parallax y={[0, 30]} x={[0, -30]} tagOuter='figure'>
-								<img src={StackWhu} alt='' className='opacity1' />
-							</Parallax>
-						</div>
-						<div>
-							<Parallax y={[0, 40]} x={[0, -40]} tagOuter='figure'>
-								<img src={StackWhu} alt='' />
-							</Parallax>
-						</div>
+						<Img
+							fluid={logo.childImageSharp.fluid}
+							alt='Logo'
+							className='banner-logo'
+							imgStyle={{ objectFit: 'contain' }}
+						/>
 
-						<div>
-							<Parallax y={[0, 50]} x={[0, -50]} tagOuter='figure'>
-								<img src={Triangle} alt='' />
-							</Parallax>
+						<h3>Creative Studio</h3>
+						<p>
+							Als eine hybride Medienproduktion entwickeln wir kreative Lösungen
+							für deine Marke, dein Unternehmen oder dein Projekt. Mit excyted
+							bringen wir unsere Disziplinen aus Web Development und der
+							Produktion von Medien zusammen und können so nicht nur
+							konzeptionell sondern auch technisch und grafisch eng mit Dir an
+							deiner Idee basteln. Bock zu excyten?
+							<br />
+							Let's get in touch!{' '}
+						</p>
+						<Link to='/projects' className='premium-button'>
+							<span className='module'>Unsere Projekte</span>
+						</Link>
+					</Col>
+					<Col md={6} className='parallaxfull'>
+						<div className='parallax-stack'>
+							<div>
+								<Parallax y={[0, 0]} x={[0, 10]} tagOuter='figure'>
+									<div className='imgequi'>
+										<img src={Dots} alt='' className='dots' />
+									</div>
+								</Parallax>
+							</div>
+							<div>
+								<Parallax y={[0, -80]} x={[0, 0]} tagOuter='figure'>
+									<img fluid={Code1} alt='' className='code1' />
+								</Parallax>
+							</div>
+							<div>
+								<Parallax y={[0, 10]} x={[0, -10]} tagOuter='figure'>
+									<Img
+										fluid={whu.childImageSharp.fluid}
+										alt=''
+										imgStyle={{ objectFit: 'contain' }}
+										className='opacity3'
+									/>
+								</Parallax>
+							</div>
+							<div>
+								<Parallax y={[0, 20]} x={[0, -20]} tagOuter='figure'>
+									<Img
+										fluid={whu.childImageSharp.fluid}
+										alt=''
+										imgStyle={{ objectFit: 'contain' }}
+										className='opacity2'
+									/>
+								</Parallax>
+							</div>
+							<div>
+								<Parallax y={[0, 30]} x={[0, -30]} tagOuter='figure'>
+									<Img
+										fluid={whu.childImageSharp.fluid}
+										alt=''
+										imgStyle={{ objectFit: 'contain' }}
+										className='opacity1'
+									/>
+								</Parallax>
+							</div>
+							<div>
+								<Parallax y={[0, 40]} x={[0, -40]} tagOuter='figure'>
+									<Img
+										fluid={whu.childImageSharp.fluid}
+										imgStyle={{ objectFit: 'contain' }}
+										alt=''
+									/>
+								</Parallax>
+							</div>
+
+							<div>
+								<Parallax y={[0, 50]} x={[0, -50]} tagOuter='figure'>
+									<img src={Triangle} alt='' />
+								</Parallax>
+							</div>
+							<div className='heighter'></div>
 						</div>
-						<div className='heighter'></div>
-					</div>
-				</Col>
-			</Row>
-		</Container>
-		<hr className='hr' />
-		<Rectangles />
-	</section>
-);
+					</Col>
+				</Row>
+			</Container>
+			<hr className='hr' />
+			<Rectangles />
+		</section>
+	);
+};
 
 export default Banner;
--- src/components/homepage/sections.js
+++ src/components/homepage/sections.js
@@ -1,144 +1,215 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { Parallax } from 'react-scroll-parallax';
+import Img from 'gatsby-image';
+import { graphql, useStaticQuery } from 'gatsby';
 import ScrollAnimation from 'react-animate-on-scroll';
 import UiAnimation from './uianimation';
 import Progressring from './ProgressRing';
 import ImageCode from '../../images/content/image-code.png';
-import ImageCar from '../../images/content/image-car.png';
-import ImageKiel from '../../images/content/image-kiel.jpg';
-import ImageThelen from '../../images/content/image-thelen.jpg';
-import ImageCaptor from '../../images/content/image-captor.jpg';
-import ImagePeople from '../../images/content/image-people.png';
-import IconArrow from '../../images/content/icon-arrow.svg';
 import IconCode from '../../images/content/code.svg';
 import IconTools from '../../images/content/tools.svg';
 import IconVisualz from '../../images/content/visualz.svg';
 import Textblock from './textblock';
 import './sections.scss';
 import 'animate.css/animate.css';
-const Sections = () => (
-	<section id='sections'>
-		<Container>
-			{/* SECTION 1 */}
-			<Row>
-				<Col md={6}>
-					<Textblock
-						title='Digitale Tools'
-						text='Die Bandbreite an digitalen Tools, die uns auf der Welt zur Verfügung stehen ist groß. Und das finden wir super! Denn wir lieben es digitale Werkzeuge effektiv einzusetzen, 
+
+const Sections = () => {
+	const { people, car, kiel, thelen, captor, code } = useStaticQuery(graphql`
+		query {
+			people: file(relativePath: { eq: "content/image-people.png" }) {
+				childImageSharp {
+					fluid(quality: 70, maxWidth: 540) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			car: file(relativePath: { eq: "content/image-car.png" }) {
+				childImageSharp {
+					fluid(quality: 70, maxWidth: 540) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			kiel: file(relativePath: { eq: "content/image-kiel.jpg" }) {
+				childImageSharp {
+					fluid(quality: 70, maxWidth: 405) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			thelen: file(relativePath: { eq: "content/image-thelen.jpg" }) {
+				childImageSharp {
+					fluid(quality: 70, maxWidth: 540) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			captor: file(relativePath: { eq: "content/image-captor.jpg" }) {
+				childImageSharp {
+					fluid(quality: 70, maxWidth: 540) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+			code: file(relativePath: { eq: "content/image-code.png" }) {
+				childImageSharp {
+					fluid(quality: 30, maxWidth: 540) {
+						...GatsbyImageSharpFluid_noBase64
+					}
+				}
+			}
+		}
+	`);
+
+	return (
+		<section id='sections'>
+			<Container>
+				{/* SECTION 1 */}
+				<Row>
+					<Col md={6}>
+						<Textblock
+							title='Digitale Tools'
+							text='Die Bandbreite an digitalen Tools, die uns auf der Welt zur Verfügung stehen ist groß. Und das finden wir super! Denn wir lieben es digitale Werkzeuge effektiv einzusetzen, 
             sodass Ideen und Fantasien keine Grenzen bei der Umsetzung gesetzt sind. Wir mischen Kreativität mit Fähigkeiten im Web Design, der Bewegtbildproduktion und unserem ständigen Drang ausdrucksvolle Projekte für Dich zu erstellen. Damit verstehen wir uns als Produzenten in einer wachsenden Omni-Channel-Welt. '
-						icon={IconTools}
-					/>
-					<div className='section-car'>
-						<img src={IconArrow} className='arrow left' alt='' />
-						<img src={ImageCar} alt='' className='image-car' />
-						<img src={IconArrow} className='arrow' alt='' />
-					</div>
-				</Col>
+							icon={IconTools}
+						/>
+						<div className='section-car'>
+							<Img
+								fluid={car.childImageSharp.fluid}
+								alt=''
+								className='image-car'
+							/>
+						</div>
+					</Col>
 
-				<Col md={6}>
-					<img className='image-people' src={ImagePeople} alt='people' />
-				</Col>
-			</Row>
+					<Col md={6}>
+						<Img
+							className='image-people'
+							fluid={people.childImageSharp.fluid}
+							alt='people'
+						/>
+					</Col>
+				</Row>
 
-			<hr class='hr' />
+				<hr class='hr' />
 
-			{/* SECTION 2 */}
-			<Row>
-				<Col md={{ size: 6, order: 2 }}>
-					<Textblock
-						title='Media'
-						text="Videos und Bilder bieten ganz besonders viel Potenzial für Excytement! Hier geht's um Feeling, Authentizität und Einzigartigkeit. Um das zu erreichen, bieten wir das volle Spektrum an Produktionsdienstleistungen an. Das beginnt bei der Konzeption, geht über die Produktion und den Schnitt und endet beim Ausrollen des Resultats über verschiedene Kanäle."
-						icon={IconVisualz}
-					/>
-					<img className='image-right' src={ImageKiel} alt='' width='75%' />
-				</Col>
-				<Col md={{ size: 6, order: 1 }}>
-					<div className='image-offset'>
-						<img src={ImageThelen} alt='' />
-						<img className='image-middle' src={ImageCaptor} alt='' />
-					</div>
-				</Col>
-			</Row>
-			<hr className='hr' />
-			{/* SECTION 3 */}
-			<Row>
-				<Col md={6}>
-					<Textblock
-						title='Web'
-						text='Eine Website hat mittlerweile jeder. Aber eine die schnell lädt, weit oben bei Google erscheint und alle Funktionen beinhaltet die Du brauchst hat nicht jeder. Das wollen wir ändern.
+				{/* SECTION 2 */}
+				<Row>
+					<Col md={{ size: 6, order: 2 }}>
+						<Textblock
+							title='Media'
+							text="Videos und Bilder bieten ganz besonders viel Potenzial für Excytement! Hier geht's um Feeling, Authentizität und Einzigartigkeit. Um das zu erreichen, bieten wir das volle Spektrum an Produktionsdienstleistungen an. Das beginnt bei der Konzeption, geht über die Produktion und den Schnitt und endet beim Ausrollen des Resultats über verschiedene Kanäle."
+							icon={IconVisualz}
+						/>
+						<Img
+							className='image-right'
+							fluid={kiel.childImageSharp.fluid}
+							alt=''
+							style={{ width: '75%' }}
+						/>
+					</Col>
+					<Col md={{ size: 6, order: 1 }}>
+						<Img
+							fluid={thelen.childImageSharp.fluid}
+							alt=''
+							className='image-left'
+						/>
+						<Img
+							className='image-middle'
+							fluid={captor.childImageSharp.fluid}
+							alt=''
+						/>
+					</Col>
+				</Row>
+				<hr className='hr' />
+				{/* SECTION 3 */}
+				<Row>
+					<Col md={6}>
+						<Textblock
+							title='Web'
+							text='Eine Website hat mittlerweile jeder. Aber eine die schnell lädt, weit oben bei Google erscheint und alle Funktionen beinhaltet die Du brauchst hat nicht jeder. Das wollen wir ändern.
 						Angefangen beim Entwickeln der grundsätzlichen Struktur deiner Seite und dem Design erstellen wir mit Dir gemeinsam eine ausdrucksvolle Website die Dich und deine Kunden excyted. '
-						icon={IconCode}
-					/>
-				</Col>
-				<Col md={6} className='ring-container'>
-					<div className='ring'>
-						<ScrollAnimation
-							animateIn='animate__fadeIn'
-							duration='1'
-							delay='0'
-							animateOnce='true'
-						>
-							<Progressring
-								radius='50'
-								stroke='6'
-								progress='94'
-								text='Performance'
-							/>
-						</ScrollAnimation>
-					</div>
-					<div className='ring'>
-						<ScrollAnimation
-							animateIn='animate__fadeIn'
-							duration='1'
-							delay='200'
-							animateOnce='true'
-						>
-							<Progressring
-								radius='50'
-								stroke='6'
-								progress='92'
-								text='Accessibility'
-							/>
-						</ScrollAnimation>
-					</div>
-					<div className='ring'>
-						<ScrollAnimation
-							animateIn='animate__fadeIn'
-							duration='1'
-							delay='400'
-							animateOnce='true'
-						>
-							<Progressring
-								radius='50'
-								stroke='6'
-								progress='100'
-								text='Best Practice'
-							/>
-						</ScrollAnimation>
-					</div>
-					<div className='ring'>
-						<ScrollAnimation
-							animateIn='animate__fadeIn'
-							duration='1'
-							delay='600'
-							animateOnce='true'
-						>
-							<Progressring radius='50' stroke='6' progress='100' text='SEO' />
-						</ScrollAnimation>
-					</div>
-				</Col>
-			</Row>
+							icon={IconCode}
+						/>
+					</Col>
+					<Col md={6} className='ring-container'>
+						<div className='ring'>
+							<ScrollAnimation
+								animateIn='animate__fadeIn'
+								duration='1'
+								delay={0}
+								animateOnce={true}
+							>
+								<Progressring
+									radius='50'
+									stroke='6'
+									progress='94'
+									text='Performance'
+								/>
+							</ScrollAnimation>
+						</div>
+						<div className='ring'>
+							<ScrollAnimation
+								animateIn='animate__fadeIn'
+								duration='1'
+								delay={200}
+								animateOnce={true}
+							>
+								<Progressring
+									radius='50'
+									stroke='6'
+									progress='92'
+									text='Accessibility'
+								/>
+							</ScrollAnimation>
+						</div>
+						<div className='ring'>
+							<ScrollAnimation
+								animateIn='animate__fadeIn'
+								duration='1'
+								delay={400}
+								animateOnce={true}
+							>
+								<Progressring
+									radius='50'
+									stroke='6'
+									progress='100'
+									text='Best Practice'
+								/>
+							</ScrollAnimation>
+						</div>
+						<div className='ring'>
+							<ScrollAnimation
+								animateIn='animate__fadeIn'
+								duration='1'
+								delay={600}
+								animateOnce={true}
+							>
+								<Progressring
+									radius='50'
+									stroke='6'
+									progress='100'
+									text='SEO'
+								/>
+							</ScrollAnimation>
+						</div>
+					</Col>
+				</Row>
 
-			<Row>
-				<Col md={3}></Col>
-				<Col md={6}>
-					<img class='image-code' src={ImageCode} alt='' width='100%' />
-					<UiAnimation />
-				</Col>
-			</Row>
-		</Container>
-	</section>
-);
+				<Row>
+					<Col md={3}></Col>
+					<Col md={6}>
+						<Img
+							className='image-code'
+							fluid={code.childImageSharp.fluid}
+							alt=''
+						/>
+						<UiAnimation />
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
+};
 
 export default Sections;
