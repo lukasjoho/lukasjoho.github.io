fb75d96a35751b9764d5eaad3abb99e5c02941e8
lukasjoho <social@lukashoppe.com>
2020-06-09 12:13:38 +0200

    Updates

--- 5e2a4920-4693034270c0e99a2c92.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{Tgqd:function(t,n,o){"use strict";o.d(n,"a",(function(){return i}));var r=o("Lnxd"),i=function(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"polygon",attr:{points:"13 19 22 12 13 5 13 19"}},{tag:"polygon",attr:{points:"2 19 11 12 2 5 2 19"}}]})(t)};i.displayName="FiFastForward"}}]);
-//# sourceMappingURL=5e2a4920-4693034270c0e99a2c92.js.map
