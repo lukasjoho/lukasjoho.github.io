77c3c34493090c53bf562ccfb1258384daa875f3
lukasjoho <social@lukashoppe.com>
2020-06-09 10:55:38 +0200

    adding user scalability in meta

--- src/components/general/layout.js
+++ src/components/general/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 			<Helmet>
 				<meta
 					name='viewport'
-					content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+					content='width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes'
 				/>
 			</Helmet>
 			<main>{children}</main>
