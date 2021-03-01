c9447a3c7b19805140c05ff31de71758fa175d73
Antonio Schmitter <antonio@excyted.io>
2021-03-01 15:48:08 +0100

    Fix breakpoint md issue

--- src/components/homepage/build.js
+++ src/components/homepage/build.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import IconDigitalLines from '../../images/content/icon-digital-lines.svg';
 import './build.scss';
-import ImageBuild from '../../images/content/build.jpg';
 import ImagePosterBuild from '../../images/content/image-build-poster.jpg';
 import VideoBuild from '../../images/content/video-build.mp4';
 
@@ -21,7 +20,6 @@ const Build = () => {
 						</h2>
 					</Col>
 					<Col lg={6} className='col-video'>
-						{/* <img src={ImageBuild} alt='' /> */}
 						<video
 							className='build-video'
 							preload='auto'
